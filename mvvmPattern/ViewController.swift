31008bf1423c9f5067306926d0db14a79e1a0dd1
Rupali Rajput <rurajput@expediagroup.com>
2022-02-22 14:35:44 +0530

    Initial Commit

---
+++ mvvmPattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvmPattern
+//
+//  Created by Rupali Rajput on 22/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
